2af58d4c21d473ba1c21f8729f8c77876c5347b2
Rahul Dominic <rahulbdominic@gmail.com>
2016-11-19 11:04:12 -0500

    Optimization library

---
+++ Optimization Library/optimize.js
@@ -0,0 +1,60 @@
+/*
+  Author: Rahul Dominic
+  Date: 19 November 2016
+*/
+
+// Requires JQuery
+$(function(){
+
+  // Constants
+  var LAMBDA = 0.01;  // Learning rate base constant
+  var GAMMA = 1;      // Learning rate exponential proportionality constant
+
+  // State variables
+  var currFloor = null;
+  var oldFloor = null;
+
+  // Prototype for State Object
+  function State(maxBid, avgBid, delta, avg_delta, floor = null) {
+    this.maxBid = maxBid;
+    this.avgBid = avgBid;
+    this.delta = delta;   // (high bid) - (floor)
+    this.avg_delta = avg_delta;     // (average bid) - (floor)
+    this.floor = floor;
+  }
+
+  // Setting initial values for data
+  function init(baseFloor, _LAMBDA, _GAMMA) {
+    currFloor = new State(null, null, null, null, baseFloor);
+    LAMBDA = _LAMBDA;
+    GAMMA = _GAMMA;
+  }
+
+  // Computes current floor based on old floor
+  function optimizeFloor() {
+    var newFloor = 0;
+    if (currFloor.delt >= 0) {
+      newFloor = currFloor.floor + currFloor.delta *
+                 (Math.pow(LAMBDA, (GAMMA / currFloor.avg_delta));
+    } else {
+      newFloor = currFloor.floor + currFloor.avg_delta *
+                 (Math.pow(LAMBDA, (GAMMA / currFloor.delta));
+    }
+    currFloor.floor = newFloor;
+  }
+
+  // Updates current state
+  function updateState(maxBid, avgBid) {
+    oldFloor = currFloor;
+    currFloor = new State (maxBid, avgBid, (maxBid - floor),
+                          (avgBid - floor), null);
+    optimizeFloor();
+
+    return currFloor;
+  }
+
+  // Returns the current state variable as State Object
+  function getCurrentState() {
+    return currFloor;
+  }
+});
