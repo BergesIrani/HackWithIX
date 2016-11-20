84b101f64d6b03bc756bbb3ecdcdbb0f1a56e075
Rahul Dominic <rahulbdominic@gmail.com>
2016-11-19 23:46:27 -0500

    Testing on lambda and gamma

--- FloorALPortal/test.js
+++ FloorALPortal/test.js
@@ -9,15 +9,16 @@ $(function(e){
   var countdown = null;
   var static_revenue = 0;
   var dynamic_revenue = 0;
-  var count = 0; // Stores the number of times called
 
   var mainEl = $('#main-content');
+
+  var profits = [];
 
   function testData(platform, format, startL, endL, startG, endG, stepL = 0.005, stepG = 0.1) {
     for(var i = startL; i < endL; i+=stepL) {
       for(var j = startG; j < endG; j+=startG) {
         var str = "";
-        str = "Lambda: " + i.toString() + "\tGamma: " + j.toString();
+        str = "Lambda: " + i.toString() + " Gamma: " + j.toString() + "</br>";
         mainEl.html(mainEl.html() + str)
         init(platform, format, i, j);
       }
@@ -25,12 +26,12 @@ $(function(e){
   }
 
   function init(platform, format, i, j) {
+    var count = 0;
     console.log("this worked");
     $.getJSON("https://raw.githubusercontent.com/rahulbdominic/HackWithIX/master/Data/parsed.json", function(jsonData) {
         console.log("JSON Data logged");
         data = jsonData[platform][format];
         $.getJSON("https://raw.githubusercontent.com/rahulbdominic/HackWithIX/master/Data/avg_stats.json", function(stat_data) {
-            console.log("JSON Data: " + stat_data);
 
               optimize = OptimizeFloor();
               optimize.init(stat_data["avg_mean"], i, j)
@@ -41,11 +42,10 @@ $(function(e){
               curr_data_point = data[count];
               var high = curr_data_point.high_bid;
               var avg = curr_data_point.avg_bid;
+              console.log("high: " + high);
 
               static_revenue += calculateRevenue(static_floor, high, avg);
               dynamic_revenue += calculateRevenue(optimize.getCurrentState().floor, high, avg);
-
-              console.log((dynamic_revenue - static_revenue) / static_revenue, optimize.getCurrentState().floor);
 
               optimize.updateState(curr_data_point.high_bid, curr_data_point.avg_bid);
 
@@ -54,7 +54,8 @@ $(function(e){
 
               if (count == 100) {
                 cancelTimer();
-                str += "\tPercent Profit: " + ((dynamic_revenue - static_revenue) / static_revenue).toString() + "\n";
+                $('#main-content2').html($('#main-content2').html() +
+                ((dynamic_revenue - static_revenue) / static_revenue).toString() + "</br>");
               }
             }
 
@@ -75,5 +76,6 @@ $(function(e){
         });
     });
   }
-  testData("app", "video", 0, 0.2, 0.8, 2.5);
+  testData("app", "video", 0, 0.2, 0.5, 2.5);
+  alert();
 });
