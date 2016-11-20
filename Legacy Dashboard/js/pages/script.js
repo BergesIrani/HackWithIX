bf9cbaa891323eaa04c13ff1721f112e2a8b086a
Rahul Dominic <rahulbdominic@gmail.com>
2016-11-19 20:33:18 -0500

    Legacy HTML/CSS/JS code commit

---
+++ Legacy Dashboard/js/pages/script.js
@@ -0,0 +1,116 @@
+﻿$(function () {
+    initVideoBiddingChart();
+    initBannerBiddingChart();
+});
+
+realtime = 'on'
+function initVideoBiddingChart() {
+    var plot = $.plot('#video_bidding_chart', [getRandomData()], {
+        series: {
+            shadowSize: 0,
+            color: 'rgb(0, 188, 212)'
+        },
+        grid: {
+            borderColor: '#f3f3f3',
+            borderWidth: 1,
+            tickColor: '#f3f3f3'
+        },
+        lines: {
+            fill: true
+        },
+        yaxis: {
+            min: 0,
+            max: 100
+        },
+        xaxis: {
+            min: 0,
+            max: 100
+        }
+    });
+
+    function updateRealTime() {
+        plot.setData([getRandomData()]);
+        plot.draw();
+
+        var timeout;
+        if (realtime === 'on') {
+            timeout = setTimeout(updateRealTime, 320);
+        } else {
+            clearTimeout(timeout);
+        }
+    }
+
+    updateRealTime();
+
+    $('#realtime').on('change', function () {
+        realtime = this.checked ? 'on' : 'off';
+        updateRealTime();
+    });
+    //====================================================================================================
+}
+
+function initBannerBiddingChart() {
+    //Real time ==========================================================================================
+    var plot = $.plot('#banner_bidding_chart', [getRandomData(), getRandomData()], {
+        series: {
+            shadowSize: 0,
+            color: 'rgb(0, 188, 212)'
+        },
+        grid: {
+            borderColor: '#f3f3f3',
+            borderWidth: 1,
+            tickColor: '#f3f3f3'
+        },
+        lines: {
+            fill: true
+        },
+        yaxis: {
+            min: 0,
+            max: 100
+        },
+        xaxis: {
+            min: 0,
+            max: 100
+        }
+    });
+
+    function updateRealTime() {
+        plot.setData([getRandomData()]);
+        plot.draw();
+
+        var timeout;
+        if (realtime === 'on') {
+            timeout = setTimeout(updateRealTime, 320);
+        } else {
+            clearTimeout(timeout);
+        }
+    }
+
+    updateRealTime();
+
+    $('#realtime').on('change', function () {
+        realtime = this.checked ? 'on' : 'off';
+        updateRealTime();
+    });
+    //====================================================================================================
+}
+
+// Feed data to charts
+var data = [], totalPoints = 110;
+function getRandomData() {
+    if (data.length > 0) data = data.slice(1);
+
+    while (data.length < totalPoints) {
+        var prev = data.length > 0 ? data[data.length - 1] : 50, y = prev + Math.random() * 10 - 5;
+        if (y < 0) { y = 0; } else if (y > 100) { y = 100; }
+
+        data.push(y);
+    }
+
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+        res.push([i, data[i]]);
+    }
+
+    return res;
+}
